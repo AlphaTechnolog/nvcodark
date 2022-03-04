9eeae02b6ae215f7c845257592e679852b7df9d0
Gabriel <gfranklings@gmail.com>
2022-03-04 06:14:24 -0400

    changed initialization messages

--- init.lua
+++ init.lua
@@ -2,5 +2,5 @@ local core = require('core')
 local ok, err = pcall(core.start)
 
 if not ok then
-  error('Cannot start neovim configuration: ' .. err)
+  error('Cannot start nvcodark (some issues detected): ' .. err)
 end
