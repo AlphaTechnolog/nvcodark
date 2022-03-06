6c6a26b355eded4f69b6d81f34f5d63a0d2e6d57
Gabriel <gfranklings@gmail.com>
2022-03-06 07:49:54 -0400

    removed lualine theme for everblush because I implement in in Mangeshrex repository && fixed some nvim-tree problems

--- lua/plugautocmds/nvim_tree.lua
+++ lua/plugautocmds/nvim_tree.lua
@@ -5,6 +5,7 @@ function M.hide_statusline ()
   local buftype = vim.api.nvim_buf_get_option(0, 'ft')
   local set_opt = vim.api.nvim_set_option
   if buftype == 'NvimTree' or buftype == 'toggleterm' then
+    vim.cmd [[ setlocal statusline=%!DisableST() ]]
     set_opt('laststatus', 0)
     return
   end
@@ -16,13 +17,12 @@ function M.autocmds ()
     vim.cmd [[ autocmd VimEnter * NvimTreeToggle ]]
   end
   if config.plugins.specify.nvim_tree.hide_statusline then
-    vim.cmd [[ autocmd BufEnter * lua require('plugautocmds.nvim_tree').hide_statusline() ]]
     vim.cmd [[
       function! DisableST()
         return " "
       endfunction
-      au BufEnter NvimTree setlocal statusline=%!DisableST()
     ]]
+    vim.cmd [[ autocmd BufEnter * lua require('plugautocmds.nvim_tree').hide_statusline() ]]
   end
 end
 
