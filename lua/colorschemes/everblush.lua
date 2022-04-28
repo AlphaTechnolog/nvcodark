10b638e1ae136ec89ccb2bd62899eda9bf020db6
Gabriel Guerra <gfranklings@gmail.com>
2022-04-28 12:14:10 -0400

    added the posibility to remove borders

--- lua/colorschemes/everblush.lua
+++ lua/colorschemes/everblush.lua
@@ -1,10 +1,21 @@
 local M = {}
 local config = require('config')
+local hi = vim.highlight.create
+
+local bg = '#181f21'
+
+function M.disable_borders ()
+  hi('VertSplit', { guifg = bg, guibg = bg }, false)
+end
 
 function M.enable ()
   require('everblush').setup({ nvim_tree = {
     contrast = config.plugins.specify.everblush.tree_contrast,
   }})
+
+  if config.plugins.specify.everblush.borders == false then
+    M.disable_borders()
+  end
 end
 
 return M
