46bfeae61d7183aad3f34fd45a96d753f3f5d04e
Gabriel Guerra <gfranklings@gmail.com>
2022-04-28 10:21:31 -0400

    removed levuaska custom contrast

--- lua/colorschemes/levuaska.lua
+++ lua/colorschemes/levuaska.lua
@@ -11,21 +11,8 @@ if not present then
   }
 end
 
-function M.apply_custom_contrast ()
-  local bg = "#0d0d14"
-  local originalbg = "#0f0f17"
-  hi('NvimTreeNormal', { guibg = bg }, false)
-  hi('NvimTreeNormalNC', { guibg = bg }, false)
-  hi('NvimTreeStatusLine', { guibg = bg }, false)
-  hi('NvimTreeStatusLineNC', { guibg = bg }, false)
-  hi('NvimTreeVertSplit', { guibg = bg, guifg = originalbg }, false)
-end
-
 function M.enable ()
-  levuaska.setup({ nvim_tree = { contrast = false } })
-  if config.plugins.specify.levuaska.nvim_tree.contrast == true then
-    M.apply_custom_contrast()
-  end
+  levuaska.setup(config.plugins.specify.levuaska)
 end
 
 return M
