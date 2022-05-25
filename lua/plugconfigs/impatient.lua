cd093a0c447f78f0801bbc9fb61e3a8bbba9a181
Gabriel Guerra <gfranklings@gmail.com>
2022-05-25 06:01:38 -0400

    best performance at starting times, enabled profiling with impatient

--- lua/plugconfigs/impatient.lua
+++ lua/plugconfigs/impatient.lua
@@ -1,5 +1,10 @@
-return {
-  setup = function ()
-    require('impatient')
+local M = {}
+local present, impatient = require('impatient')
+
+function M.setup ()
+  if present then
+    impatient.enable_profile()
   end
-}
+end
+
+return M
