f69e35291905a4e678f5a98fcf96be83b94c7704
Gabriel Guerra <gfranklings@gmail.com>
2022-06-05 18:04:14 -0400

    added the ctrl-b mapping

--- lua/mappings/buffers.lua
+++ lua/mappings/buffers.lua
@@ -2,4 +2,4 @@ local map = require('utils.map')
 
 map.set('n', '<Tab>', map.cmd('BufferNext'))
 map.set('n', '<S-Tab>', map.cmd('BufferPrevious'))
-map.set('n', '<C-A-b>', map.cmd('BufferClose'))
+map.set('n', '<C-b>', map.cmd('BufferDelete'))
