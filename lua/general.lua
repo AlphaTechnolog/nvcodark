e67fa87bfb55a45f1ceea693a4b8d935b9d9300f
Gabriel Guerra <gfranklings@gmail.com>
2022-06-09 20:17:47 -0400

    simplified general variables loader logic

--- lua/general.lua
+++ lua/general.lua
@@ -1,14 +1,10 @@
 local config = require('config')
 local M = {}
 
-function M.load_values (obj, conf)
-  for name, value in pairs(conf) do
-    obj[name] = value
-  end
-end
-
 for optname, optconf in pairs(config.general) do
-  M.load_values(vim[optname], optconf)
+  for name, value in pairs(optconf) do
+    vim[optname][name] = value
+  end
 end
 
 return M
