2456fde97757fb89e5d4d621954b6c06b192ee1a
Gabriel Guerra <gfranklings@gmail.com>
2022-10-09 12:45:16 -0400

    space typo

--- lua/nvcodark/plugins/default_plugs.lua
+++ lua/nvcodark/plugins/default_plugs.lua
@@ -144,7 +144,7 @@ return {
       config = function ()
          require 'indent_blankline'.setup {
             show_current_context = true,
-            show_current_context_start =true 
+            show_current_context_start = true
          }
       end
    },
