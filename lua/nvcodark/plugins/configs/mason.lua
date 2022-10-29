a60711fe71479bd8278736e60fc62dce1dafc8b1
Gabriel Guerra <gfranklings@gmail.com>
2022-10-29 11:29:33 -0400

    cmp: fixed deprecation issues

--- lua/nvcodark/plugins/configs/mason.lua
+++ lua/nvcodark/plugins/configs/mason.lua
@@ -28,9 +28,7 @@ local function load_lspconfig ()
    end
 
    -- making capabilities
-   local capabilities = require('cmp_nvim_lsp').update_capabilities(
-      vim.lsp.protocol.make_client_capabilities()
-   )
+   local capabilities = require 'cmp_nvim_lsp'.default_capabilities()
 
    -- enable lsp saga, also enables symbols in winbar
    local saga = require 'lspsaga'
